35b1abbd0ebcc58eec19b72485b4f2e9ae9c885a
Yoopster <joepbarmentlo@gmail.com>
2020-02-28 13:05:45 +0100

    main now launches a game vs AI after training. defined objectives in research

--- srcs/main.c
+++ srcs/main.c
@@ -8,14 +8,14 @@ int main(void) {
 	int turn;
 	int it = 1;
 	
-	while (it <= 5)
+	while (it <= 10)
 	{
 		learn(2000);
-//		show_game();
-//		printf("trained for %d games\n", (it * 2000));
+		show_game();
+		printf("trained for %d games\n", (it * 2000));
 		it++;
 	}
-	
-//	play_against_AI();
+	//printf("non zeroes: %d\n", count_non_zeroes_db());
+	play_against_AI();
   return (0);
 }
